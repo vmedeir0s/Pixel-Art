092b92c467ad62dfa7cdd75f160b6a421ac776a6
Vinicius de Medeiros <vmeedeiros@gmail.com>
2022-02-15 18:53:47 -0300

    Adiciona requisito 7

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const positionPixelBoard = document.getElementById('pixel-board');
-function boxPixel() {
-  for (let i = 0; i < 5; i += 1) {
+function boxPixel(value = 5) {
+  for (let i = 0; i < value; i += 1) {
     const linha = document.createElement('div');
     positionPixelBoard.appendChild(linha);
-    for (let j = 0; j < 5; j += 1) {
+    for (let j = 0; j < value; j += 1) {
       const pixel = document.createElement('div');
       pixel.className = 'pixel';
       linha.appendChild(pixel);
@@ -12,29 +12,29 @@ function boxPixel() {
 }
 boxPixel();
 
-// function removeClassSelected(event) {
-//   if (event.target.classList.contains('selected')) {
-//     event.target.classList.remove('selected');
-//     console.log('clickou');
-//   }
-// }
+function addClassSelect() {
+  const positionPaletColor = document.querySelector('#color-palette');
+  positionPaletColor.addEventListener('click', function(event) {
+    let positionSelect = document.querySelector('.selected');
+    positionSelect.classList.remove('selected');
+    event.target.classList.add('selected');
+  });
+}
+addClassSelect();
 
-// function addClassSelected() {
-//   for (let i = 0; i < vetorCores.length; i += 1) {
-//     vetorCores[i].addEventListener('click', removeClassSelected(event));
-//   }
-// }
-// addClassSelected();
+function addBackgroundOnPixel() {
+  let positionSelect = document.querySelector('.selected');
+  
+}
 
-window.onload = function () {
-  function geraCor() {
-    const cor = Math.floor(Math.random() * 16777215).toString(16);
-    return cor;
-  }
-  const corBox2 = document.querySelector('#box2');
-  const corBox3 = document.querySelector('#box3');
-  const corBox4 = document.querySelector('#box4');
-  corBox2.style.backgroundColor = `#${geraCor()}`;
-  corBox3.style.backgroundColor = `#${geraCor()}`;
-  corBox4.style.backgroundColor = `#${geraCor()}`;
-};
+
+function geraCor() {
+  const cor = Math.floor(Math.random() * 16777215).toString(16);
+  return cor;
+}
+const corBox2 = document.querySelector('#box2');
+const corBox3 = document.querySelector('#box3');
+const corBox4 = document.querySelector('#box4');
+corBox2.style.backgroundColor = `#${geraCor()}`;
+corBox3.style.backgroundColor = `#${geraCor()}`;
+corBox4.style.backgroundColor = `#${geraCor()}`;
