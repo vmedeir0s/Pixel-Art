367f528df83ec4bd906d7e520c92fda8b6b19862
Vinicius de Medeiros <vmeedeiros@gmail.com>
2022-02-15 17:55:41 -0300

    Adiciona requisito bonus 12

--- script.js
+++ script.js
@@ -25,3 +25,16 @@ boxPixel();
 //   }
 // }
 // addClassSelected();
+
+window.onload = function () {
+  function geraCor() {
+    const cor = Math.floor(Math.random() * 16777215).toString(16);
+    return cor;
+  }
+  const corBox2 = document.querySelector('#box2');
+  const corBox3 = document.querySelector('#box3');
+  const corBox4 = document.querySelector('#box4');
+  corBox2.style.backgroundColor = `#${geraCor()}`;
+  corBox3.style.backgroundColor = `#${geraCor()}`;
+  corBox4.style.backgroundColor = `#${geraCor()}`;
+};
